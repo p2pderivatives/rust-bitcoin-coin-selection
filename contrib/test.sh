5b1a852bbbaee785d3ba3d08c41c49cbac168b23
yancy <yancy@yancy.lol>
2022-12-14 15:15:53 +0100

    Add basic CI setup script

---
+++ contrib/test.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+# CI script for the `coin selection` crate.
+
+set -ex
+
+FEATURES="rand"
+
+# Sanity checks.
+cargo --version
+rustc --version
+
+cargo build --all-features
+cargo test --all-features
+
+if [ "$DO_FEATURE_MATRIX" = true ]; then
+    # No features.
+    cargo build --no-default-features
+    cargo test --no-default-features
+
+    # Single features.
+    for feature in ${FEATURES}
+    do
+        cargo build --no-default-features --features="$feature"
+        cargo test --no-default-features --features="$feature"
+    done
+fi
