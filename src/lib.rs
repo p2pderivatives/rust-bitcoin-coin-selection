30588534c0697ba515834e3de20c5f790c9d870b
yancy <github@yancy.lol>
2025-06-25 15:09:39 -0500

    Add `max_weight` param to `BnB`
    If a selected `UTXO` would exceed the `max_weight` then do not include
    that `UTXO` in the selection and backtrack.  Note that the selection is
    sorted by `effective_value` with `weight` as a tie breaker.  Therefore,
    there is no guarantee that a minimum weight solution is found unlike
    coin-grinder.
    
    Test `select_coins_bnb_max_weight_yields_no_solution` sets a target that
    would be attainable but fails because the only candidate `UTXOs` exceed
    the `max_weight`.
    
    Test `select_coins_bnb_max_weight_without_error` sets a target that
    could be met only by the final `UTXO`.  Therefore this tests that the
    backtrack step subtracts the first `UTXO` weight and value before
    proceeding.  The final `UTXOs` weight alone is less than `max_weight`
    allowing the target to be met.
    
    Lastly the test `select_coins_bnb_utxo_pool_weight_overflow` shows that
    no panic occurs if the weight sum exceeds `u64::MAX`.  Also, the `BnB`
    prop-test is augmented to catch the case where `max_weight` is exceeded.
    
    It's notable that the benchmark times are significantly slower with this
    change.  That seems reasonable since there is now an additional check
    per iteration for `max_weight`.  Furthermore during backtrack there is an
    additional operation to subtract the weight from the total.

--- src/lib.rs
+++ src/lib.rs
@@ -165,7 +165,7 @@ pub fn select_coins(
     max_weight: Weight,
     weighted_utxos: &[WeightedUtxo],
 ) -> Return<'_> {
-    let bnb_result = select_coins_bnb(target, cost_of_change, weighted_utxos);
+    let bnb_result = select_coins_bnb(target, cost_of_change, max_weight, weighted_utxos);
 
     if bnb_result.is_err() {
         select_coins_srd(target, max_weight, weighted_utxos, &mut thread_rng())
