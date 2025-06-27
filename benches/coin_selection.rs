3d755ae792fe7fc0d99956af227df7a826942abc
yancy <github@yancy.lol>
2025-06-27 06:52:36 -0500

    refactor: apply fmt

--- benches/coin_selection.rs
+++ benches/coin_selection.rs
@@ -17,15 +17,9 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     // https://github.com/bitcoin/bitcoin/blob/f3bc1a72825fe2b51f4bc20e004cef464f05b965/src/wallet/coinselection.h#L18
     let cost_of_change = Amount::from_sat(50_000);
 
-    let one = Utxo {
-        value: Amount::from_sat(1_000),
-        weight: Weight::ZERO,
-    };
+    let one = Utxo { value: Amount::from_sat(1_000), weight: Weight::ZERO };
 
-    let two = Utxo {
-        value: Amount::from_sat(3),
-        weight: Weight::ZERO,
-    };
+    let two = Utxo { value: Amount::from_sat(3), weight: Weight::ZERO };
 
     let target = Amount::from_sat(1_003);
     let mut utxo_pool = vec![one; 1000];
