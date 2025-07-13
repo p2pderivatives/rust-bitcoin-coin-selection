7740e1f214c3b111b9e131c5ebc3fb3920078efc
yancy <github@yancy.lol>
2025-07-13 11:10:01 -0500

    test: Add `SRD` benchmark
    Provide feedback for changes as to their performance impact.

--- README.md
+++ README.md
@@ -48,10 +48,11 @@ Note: criterion requires rustc version 1.65 to run the benchmarks.
 
 ### performance comparison
 
-A basic performance comparison between this Rust BnB implementation and [Bitcoin Core](https://github.com/bitcoin/bitcoin/blob/4b1196a9855dcd188a24f393aa2fa21e2d61f061/src/wallet/coinselection.cpp#L76) using commodity hardware (My rather old laptop).
+A basic performance comparison between implementations using commodity hardware (My rather old laptop).
 
 |implementation|pool size|ns/iter|
 |-------------:|---------|-------|
+|      Rust SRD|    1,000| 32,340|
 |      Rust BnB|    1,000|499,600|
 |  C++ Core BnB|    1,000|816,374|
 
---
+++ benches/srd_selection.rs
@@ -0,0 +1,35 @@
+use bitcoin_coin_selection::{select_coins_srd, WeightedUtxo};
+use bitcoin_units::{Amount, FeeRate, Weight};
+use criterion::{criterion_group, criterion_main, Criterion};
+use rand::thread_rng;
+
+#[derive(Debug, Clone)]
+pub struct Utxo {
+    value: Amount,
+    weight: Weight,
+}
+
+impl WeightedUtxo for Utxo {
+    fn weight(&self) -> Weight { self.weight }
+    fn value(&self) -> Amount { self.value }
+}
+
+pub fn srd_benchmark(c: &mut Criterion) {
+    let utxo = Utxo { value: Amount::from_sat_u32(100), weight: Weight::ZERO };
+    let pool = vec![utxo; 1_000];
+
+    let target = Amount::from_sat_u32(50_000);
+    let fee_rate = FeeRate::from_sat_per_kwu(10);
+
+    c.bench_function("srd", |b| {
+        b.iter(|| {
+            let (iteration_count, inputs) =
+                select_coins_srd(target, fee_rate, &pool, &mut thread_rng()).unwrap();
+            assert_eq!(iteration_count, 1_000);
+            assert_eq!(inputs.len(), 1_000);
+        })
+    });
+}
+
+criterion_group!(benches, srd_benchmark);
+criterion_main!(benches);
