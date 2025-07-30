8d86155eeac2c4cd0cbcbbc772cb41056bb62c7c
yancy <github@yancy.lol>
2025-07-30 12:10:42 -0500

    selection: move parameters from function call into `UTXO` field
    Make code more dry by storing `effective_value` and `waste_score` as a
    property of `WeightedUtxo`,  Therefore, each algorithm need not
    independently calculate the required properties.
    
    Add fee-rate and long-term fee-rate to `WeightedUtxo` since it is
    required to calculate the `effective_value`.  Since the fee-rate
    parameters are passed to `WeightedUtxo`, they no longer need be passed
    to each algorithm.  This also has another pleasant side-effect in that
    it matches how bitcoin-core works having in that the fee-rate and
    long-term fee-rate can be used to store the fee and long-term fee
    of the `UTXO`.
    
    By adding these parameters to the `UTXO` constructor, this makes it
    impossible to create a `UTXO` which has a negative effective value.
    This simplifies the logic of the selection algorithms and also
    invalidates some tests that assert the behavior of handling negative
    valued `UTXOs`.
    
    Now that the fee-rates are used to store and calculate the fee values,
    only `WeightedUtoxs` that have a valid fee will be created.  Therefore,
    the waste calculation can no longer overflow.  Therefore, the checks
    during waste calculation are removed.
    
    A marginal performance improvement for `BnB` and a large improvement for
    `SRD` is recorded.  The large improvement in `SRD` reflects that the
    effective-value is not recalculated in loop and also there is no
    longer a needed check that effective-value is a positive value within
    bounds.

--- benches/srd_selection.rs
+++ benches/srd_selection.rs
@@ -4,16 +4,18 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use rand::thread_rng;
 
 pub fn srd_benchmark(c: &mut Criterion) {
-    let utxo = WeightedUtxo::new(Amount::from_sat_u32(100), Weight::ZERO);
+    let fee_rate = FeeRate::from_sat_per_kwu(10);
+    let lt_fee_rate = FeeRate::MAX;
+    let utxo =
+        WeightedUtxo::new(Amount::from_sat_u32(100), Weight::ZERO, fee_rate, lt_fee_rate).unwrap();
     let pool = vec![utxo; 1_000];
 
     let target = Amount::from_sat_u32(50_000);
-    let fee_rate = FeeRate::from_sat_per_kwu(10);
 
     c.bench_function("srd", |b| {
         b.iter(|| {
             let (iteration_count, inputs) =
-                select_coins_srd(target, fee_rate, &pool, &mut thread_rng()).unwrap();
+                select_coins_srd(target, &pool, &mut thread_rng()).unwrap();
             assert_eq!(iteration_count, 1_000);
             assert_eq!(inputs.len(), 1_000);
         })
