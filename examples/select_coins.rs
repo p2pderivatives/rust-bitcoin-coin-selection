35088921e401f4a7b7c2f43d32b8c3b108b00204
yancy <github@yancy.lol>
2026-07-12 17:59:13 -0500

    examples: improve api example for 0.x.x lineage

--- examples/select_coins.rs
+++ examples/select_coins.rs
@@ -1,9 +1,8 @@
-use std::collections::HashSet;
-
 use bitcoin::{Amount, FeeRate, Weight};
-use bitcoin_coin_selection::{branch_and_bound, select_coins, single_random_draw, WeightedUtxo};
+use bitcoin_coin_selection::{branch_and_bound, select_coins, single_random_draw, single_random_draw_with_rng, WeightedUtxo};
+use rand::thread_rng;
 
-#[derive(Debug, Eq, Hash, PartialEq)]
+#[derive(Debug, Eq, PartialEq)]
 struct Utxo {
     value: Amount,
 }
@@ -22,20 +21,29 @@ fn main() {
     let amts = [271_828, 314_159];
     let utxos: Vec<_> = amts.iter().map(|&x| Utxo { value: Amount::from_sat(x) }).collect();
 
-    let select_coins =
-        select_coins(target, cost_of_change, fee_rate, long_term_fee_rate, &utxos).unwrap();
-
     let bnb_selection =
         branch_and_bound(target, cost_of_change, fee_rate, long_term_fee_rate, &utxos);
-    let srd_selection = single_random_draw(target, fee_rate, &utxos).unwrap();
+    assert!(bnb_selection.is_none());
 
-    if let Some(selection) = bnb_selection {
-        assert_eq!(selection, select_coins);
+    let srd_selection = single_random_draw(target, fee_rate, &utxos);
+    if let Some((i, selection)) = srd_selection {
+        println!("found selection {:?} found in {:?} iterations", selection, i);
     } else {
-        let srd_set: HashSet<_> = srd_selection.1.into_iter().collect();
-        let select_set: HashSet<_> = select_coins.1.into_iter().collect();
-        // since these are randomized results, compare only the set such
-        // that result order does not matter.
-        assert_eq!(srd_set, select_set);
+        panic!("expected selection found");
+    }
+
+    let srd_selection = single_random_draw_with_rng(target, fee_rate, &utxos, &mut thread_rng());
+    if let Some((i, selection)) = srd_selection {
+        println!("found selection {:?} found in {:?} iterations", selection, i);
+    } else {
+        panic!("expected selection found");
+    }
+
+    let coins_selection =
+        select_coins(target, cost_of_change, fee_rate, long_term_fee_rate, &utxos);
+    if let Some((i, selection)) = coins_selection {
+        println!("found selection {:?} found in {:?} iterations", selection, i);
+    } else {
+        panic!("expected selection found");
     }
 }
