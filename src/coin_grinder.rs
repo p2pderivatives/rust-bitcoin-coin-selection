102c09d2320033f7f3ad9a9cb6bd2904a02be2e1
yancy <github@yancy.lol>
2026-07-23 16:24:48 -0500

    algo: coin-grinder returns no solution if target amount is zero

--- src/coin_grinder.rs
+++ src/coin_grinder.rs
@@ -148,7 +148,7 @@ pub fn coin_grinder<'a, T: IntoIterator<Item = &'a WeightedUtxo> + std::marker::
         return Err(InsufficentFunds);
     }
 
-    if weighted_utxos.is_empty() {
+    if weighted_utxos.is_empty() || target == Amount::ZERO {
         return Err(SolutionNotFound);
     }
 
@@ -645,6 +645,21 @@ mod tests {
             max_weight: "0",
             fee_rate: "0",
             weighted_utxos: &[],
+            expected_utxos: &[],
+            expected_error: Some(SolutionNotFound),
+            expected_iterations: 0,
+        }
+        .assert();
+    }
+
+    #[test]
+    fn target_zero_with_available_value() {
+        TestCoinGrinder {
+            target: "0",
+            change_target: "0",
+            max_weight: "1000",
+            fee_rate: "10 sat/vB",
+            weighted_utxos: &["1000 sats/230 wu"],
             expected_utxos: &[],
             expected_error: Some(SolutionNotFound),
             expected_iterations: 0,
