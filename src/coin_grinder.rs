2513eed4310adc223121dedc7c16ec530978a05b
yancy <github@yancy.lol>
2026-07-30 15:51:46 -0500

    docs: fee rate is two words

--- src/coin_grinder.rs
+++ src/coin_grinder.rs
@@ -98,7 +98,7 @@ fn is_remaining_weight_higher(
 ///
 /// See discussion [here](https://murch.one/erhardt2016coinselection.pdf) at section 6.4.3
 /// that prioritizing input weight will lead to a fragmentation of the UTXO set.  Therefore, prefer
-/// this search when fee_rate is high, since a set of UTXOs with minimal weight will lead to a
+/// this search when fee rate is high, since a set of UTXOs with minimal weight will lead to a
 /// cheaper constructed transaction in the short term.  However, in the long-term, this
 /// prioritization can lead to more UTXOs to choose from (fragmentation).
 ///
