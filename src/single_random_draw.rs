b5292e00e2abe6ccc3ef5d05cfbc3ee4cccb5c16
yancy <github@yancy.lol>
2026-08-10 08:10:19 -0500

    test: remove duplicate test

--- src/single_random_draw.rs
+++ src/single_random_draw.rs
@@ -212,14 +212,6 @@ mod tests {
 
     #[test]
     #[should_panic]
-    // the target is greater than the sum of available UTXOs.
-    // therefore asserting that a selection exists should panic.
-    fn select_coins_srd_eleven_invalid_target_should_panic() {
-        assert_coin_select("11 cBTC", 8, &["1 cBTC"]);
-    }
-
-    #[test]
-    #[should_panic]
     fn select_coins_srd_params_invalid_target_should_panic() {
         // the target is greater than the sum of available UTXOs.
         // therefore asserting that a selection exists should panic.
